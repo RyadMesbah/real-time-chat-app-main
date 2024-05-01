5bc15c4b096b69fdee9e415f477230e921e1c076
Mesbah Ryad <ryad.mesbah03@gmail.com>
2024-05-01 19:35:30 +0100

    pfe

---
+++ real-time-chat-app-main/models/message.model.js
@@ -0,0 +1,36 @@
+import mongoose from "mongoose";
+const { Schema } = mongoose;
+const messageSchema = new Schema(
+  {
+    messageId: {
+      type: String,
+      required: true,
+    },
+    contenu: {
+      type: String,
+      required: true,
+    },
+
+    date_heure_envoie: {
+      type: String,
+      required: true,
+    },
+    userId: {
+      type: String,
+      required: true,
+    },
+    roomID: {
+      type: String,
+      required: true,
+    },
+    is_message_pinned: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Room = mongoose.model("Message", RoomSchema);
